ba199d7bf510eaf0ed0567d4a4460f427ab851ce
dwikbtn <dwikbtn15@gmail.com>
2021-11-02 20:46:20 +0800

    adding latest news

--- components/elements/LatestNews.tsx
+++ components/elements/LatestNews.tsx
@@ -4,11 +4,11 @@ export const LatestNews = (): JSX.Element => {
   return (
     <div
       id="latest-article"
-      className="container grid gap-8 grid-cols-3 grid-rows-2"
+      className="container grid gap-8 grid-cols-3 grid-rows-2 mt-10 "
     >
-      <article className="col-start-1 col-end-4">
-        <div className="w-full max-h-60 border shadow flex ">
-          <div id="content" className="pt-10 pb-11 px-10">
+      <article className="col-start-1 col-end-4 max-h-6">
+        <div className="w-full min-h-60 border shadow flex ">
+          <div id="content" className="pt-10 pb-11 px-10 w-3/4">
             <h1 className="font-bold text-3xl">Title</h1>
             <p className="pt-6 text-gray-600">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
@@ -22,15 +22,105 @@ export const LatestNews = (): JSX.Element => {
               </a>
             </div>
           </div>
-          <Image
-            src="https://placeimg.com/640/480/tech"
-            alt="image placeholder"
-          />
+          <div className="relative w-1/2">
+            <Image
+              src="https://placeimg.com/640/480/tech "
+              alt="image placeholder"
+              layout="fill"
+            />
+          </div>
         </div>
       </article>
-      <article>2</article>
-      <article>3</article>
-      <article>4</article>
+      <article>
+        <div
+          id="content"
+          className="w-full border shadow flex flex-row flex-wrap justify-center items-center text-center"
+        >
+          <div id="image-wrapper" className="relative w-full">
+            <Image
+              src="https://placeimg.com/640/480/tech "
+              alt="image placeholder"
+              width="300"
+              height="165"
+              className="w-full object-cover"
+            />
+          </div>
+          <div className="w-full p-4">
+            <h1 className="font-bold text-3xl">Title</h1>
+            <p className="pt-6 text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+              totam. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Veniam, possimus.
+            </p>
+            <div className="flex justify-between pt-7 text-gray-600">
+              <p>12/12/12</p>
+              <a href="http://" className="font-semibold">
+                Read More
+              </a>
+            </div>
+          </div>
+        </div>
+      </article>
+      <article>
+        <div
+          id="content"
+          className="w-full border shadow flex flex-row flex-wrap justify-center items-center text-center"
+        >
+          <div id="image-wrapper" className="relative w-full">
+            <Image
+              src="https://placeimg.com/640/480/tech "
+              alt="image placeholder"
+              width="300"
+              height="165"
+              className="w-full object-cover"
+            />
+          </div>
+          <div className="w-full p-4">
+            <h1 className="font-bold text-3xl">Title</h1>
+            <p className="pt-6 text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+              totam. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Veniam, possimus.
+            </p>
+            <div className="flex justify-between pt-7 text-gray-600">
+              <p>12/12/12</p>
+              <a href="http://" className="font-semibold">
+                Read More
+              </a>
+            </div>
+          </div>
+        </div>
+      </article>
+      <article>
+        <div
+          id="content"
+          className="w-full border shadow flex flex-row flex-wrap justify-center items-center text-center"
+        >
+          <div id="image-wrapper" className="relative w-full">
+            <Image
+              src="https://placeimg.com/640/480/tech "
+              alt="image placeholder"
+              width="300"
+              height="165"
+              className="w-full object-cover"
+            />
+          </div>
+          <div className="w-full p-4">
+            <h1 className="font-bold text-3xl">Title</h1>
+            <p className="pt-6 text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+              totam. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Veniam, possimus.
+            </p>
+            <div className="flex justify-between pt-7 text-gray-600">
+              <p>12/12/12</p>
+              <a href="http://" className="font-semibold">
+                Read More
+              </a>
+            </div>
+          </div>
+        </div>
+      </article>
     </div>
   );
 };
--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["https://placeimg.com/"],
+    domains: ["placeimg.com"],
   },
 };
